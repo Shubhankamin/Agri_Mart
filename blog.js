d0622f26a9494dd4ec679557a712cd84e15f5fc7
Aaryas04 <aarya04@gmail.com>
2025-10-08 14:03:03 +0530

    Additional changes

--- blog.js
+++ blog.js
@@ -2,7 +2,7 @@ const blogs = {
   featured: {
     title: "How to Make Extra Money from Your Farm",
     date: "8/10/2025 07:52 AM",
-    image: "Images/blog1.jpg",
+    image: "Images_A/blog1.jpg",
     description:
       "Discover how farmers can boost income by selling surplus produce online and adopting modern marketing strategies.",
   },
@@ -10,21 +10,21 @@ const blogs = {
     {
       title: "7 Big Things a Start-up Must Have to Succeed",
       date: "13/08/2025 07:52 AM",
-      image: "Images/blog2.jpg",
+      image: "Images_A/blog2.jpg",
       description:
         "Key steps to launch a successful agri-startup and make a lasting impact in the agricultural sector.",
     },
     {
       title: "9 Steps to Starting an Agribusiness",
       date: "18/01/2025 07:52 AM",
-      image: "Images/blog3.jpg",
+      image: "Images_A/blog3.jpg",
       description:
         "A practical guide to starting your own agribusiness and connecting directly with consumers.",
     },
     {
       title: "10 Rules to Build a Sustainable Future",
       date: "28/12/2024 07:52 AM",
-      image: "Images/blog4.jpg",
+      image: "Images_A/blog4.jpg",
       description:
         "Learn how eco-friendly farming practices can transform the agricultural industry for good.",
     },
